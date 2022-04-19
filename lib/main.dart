0a1b7476ae53170e5ad31fd1c6a737799463ff3b
Tomislav Medved <tmedved@avalanchedev.com>
2022-04-19 15:16:09 +0200

    added splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        // fontFamily: 'Karla',
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
